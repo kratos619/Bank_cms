18016bb9deab19880b442bf9dbab4a480315d67a
kratos619 <gaurav.palaspagar@hotmail.com>
2018-06-05 10:59:32 +0530

    add db connection add db connect and disconnect functions

---
+++ private/db_credential.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", null);
+define("DB_NAME", "globe_bank");
+
+ ?>
--- private/shared/staff-footer.php
+++ private/shared/staff-footer.php
@@ -3,6 +3,10 @@
     <footer class="bg-info" style="padding-bottom: 40px;  margin-top: 25px;" >
     &copy; <?php echo date('Y'); ?> Globe Bank
     </footer>
-</div>
+</div>  `
 </body>
 </html>
+
+<?php
+db_disconnect($db);
+?>
