6803337cd89feef8c13f2326ed3f4a957e6e79ed
kratos619 <gaurav.palaspagar@hotmail.com>
2018-06-07 23:50:57 +0530

    page CRUD (correction - 3)

--- public/staff/pages/edit.php
+++ public/staff/pages/edit.php
@@ -41,17 +41,20 @@ if (!isset($_GET['id'])) {
     $selected_id = $_GET['id'];
 
     if(request_is_post()){
+      $page = [];
+      $selected_id['id'] = $_POST['id'];
         $selected_menu['menu_name']= $_POST['menu_name'];
         $selected_subject_id['subject_id']= $_POST['subject_id'];
         $selected_position['position']= $_POST['position'];
         $selected_visibility['visible']= $_POST['visible'];
-
+$selected_content ['content'] = $_POST['content'];
 
         $sql = "update pages set ";
         $sql .= "subject_id='" . $selected_subject_id['subject_id'] . "', ";
         $sql .= "menu_name='" . $selected_menu['menu_name'] . "', ";
         $sql .= "position='" .  $selected_position['position']. "', ";
         $sql .= "visible='" . $selected_visibility['visible'] . "' ";
+        $sql .= "content='" . $selected_content['content'] . "' ";
         $sql .= "where id='" . $selected_id . "' ";
         $sql .= "limit 1";
 
@@ -69,7 +72,7 @@ if (!isset($_GET['id'])) {
 
 
     }else{
-        $selected_page = find_pages_by_id($selected_id);
+        $page = find_pages_by_id($selected_id);
         $page_set = find_all_pages();
         $page_count= mysqli_num_rows($page_set);
         mysqli_free_result($page_set);
@@ -81,7 +84,7 @@ if (!isset($_GET['id'])) {
     <form action="<?php echo url_for('staff/pages/edit.php?id='. $selected_id); ?>" method="post">
         <div class="form-group">
             <label for="">Menu Name</label>
-            <input type="text" name="menu_name" class="form-control" value="<?php echo $selected_page['menu_name']; ?>"/>
+            <input type="text" name="menu_name" class="form-control" value="<?php echo h($page['menu_name']); ?>"/>
         </div>
         <div class="form-group">
             <label>Select Position</label>
@@ -89,7 +92,7 @@ if (!isset($_GET['id'])) {
                 <?php
                 for ($i = 1 ; $i <= $page_count ; $i++){
                     echo "<option value=\"{$i}\"";
-                    if($selected_page['position'] == $i){
+                    if($page['position'] == $i){
                         echo " selected ";
                     }echo ">{$i}</option>";
                 }
@@ -100,20 +103,20 @@ if (!isset($_GET['id'])) {
         </div>
         <div class="form-group">
             <label>Select Subject Id</label>
-            <select name="subject_id" class="form-control" id="exampleFormControlSelect1">
+            <select name="subject_id" class="form-control" >
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
             </select>
         </div>
         <div class="form-group">
-            <label for="exampleFormControlTextarea1">Add Content</label>
-            <textarea  name="content" class="form-control" id="exampleFormControlTextarea1" rows="6"><?php echo h($selected_page['content']);?></textarea>
+            <label>Add Content</label>
+            <textarea  name="content" class="form-control" id="exampleFormControlTextarea1" rows="6"><?php echo h($page['content']);?></textarea>
         </div>
         <div style="margin-left:20px;" class="form-check">
             <input type="hidden" name="visible" value="0" class="form-check-input">
-            <input type="checkbox" name="visible" value="1" class="form-check-input" >
-            <label class="form-check-label" for="exampleCheck1">Visible</label>
+            <input type="checkbox" <?php if($page['visible'] == '1' ){ echo "checked" ; } ?>  name="visible" value="1" class="form-check-input" >
+            <label class="form-check-label"  >Visible</label>
         </div>
         <input type="submit" value="Update Page" class="btn btn-primary" />
     </form>
